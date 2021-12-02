db51fbe504e677954cda2bc128e9bb7f1fc04a80
KacperPuzi0 <79982696+KacperPuzi0@users.noreply.github.com>
2021-12-02 22:37:33 +0100

    +

---
+++ Dzielenie ciasta/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace Dzielenie_ciasta
+{
+    class Program
+    {
+        public static long ileKawalkow(long n)
+        {
+            if (n <= 1)
+            {
+                return 0;
+            }
+            double delta = Math.Sqrt((n*2 - 2 )*4 + 1)-2;
+            if (delta % 2 != 0)
+            {
+                return Convert.ToInt64(delta) / 2 + 1;
+            }
+            return Convert.ToInt64(delta) / 2;
+        }
+
+
+
+
+        static void Main()
+        {
+            var results = new StringBuilder();
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                results.AppendLine(ileKawalkow(long.Parse(Console.ReadLine())).ToString());
+            }
+            Console.WriteLine(results.ToString());
+        }
+    }
+}
